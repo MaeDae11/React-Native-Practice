ab514d83b1ccf34e5cbe1cd22d3e9c70cf577c4b
Katie <katherinelane11@gmail.com>
2017-09-19 13:58:30 -0400

    initial upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HELLO WORLD</Text>
+        <Text>This is my first React Native App</Text>
+        <Text>SHAKE MEEEEE</Text>
+        <Text>Jk. Please don't</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
